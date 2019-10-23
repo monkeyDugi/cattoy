80006cc442fa3c628cdecce4acf8503050307852
monkeyDugi <lbd4946@gmail.com>
2019-10-23 21:49:33 +0900

    Mock객체 활용

--- src/test/java/com/dallab/cattoy/controller/GreetingControllerTest.java
+++ src/test/java/com/dallab/cattoy/controller/GreetingControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,13 +36,23 @@ public class GreetingControllerTest {
     @Autowired // new MockMvc 이거를 안해주는거
     private MockMvc mockMvc;
 
-    @SpyBean // @WebMvcTest에서 지정한 클래스만 테스트가 가능하기 때문에,
+    //@SpyBean // @WebMvcTest에서 지정한 클래스만 테스트가 가능하기 때문에,
              // @SpyBean사용 : 진짜 객체며, Controller에서 GreetingService를 포함하기 때문에 이렇게 사용해 주어야 한다.
              // 그렇지 않으면 Service를 통과히지 못한다.
-    //@MockBean // 가짜 객체, MockBean객체를 테스트 하기 위해서는 given을 사용해야 한다.
+    @MockBean // 가짜 객체, MockBean객체를 테스트 하기 위해서는 given을 사용해야 한다.
     private GreetingService greetingService;
 
-    //@Before
+    // MockBean은 가짜 객체로써 실제 Service객체를 생성하지 않고 통과만 시키는 것이다.
+    // 즉, DB까지 연동 된다면, 실제 DB를 불러오지 않고 형태만 갖추고 있는 것이다. 그러면 DB까지 갈 이유가 없다면
+    // 효율적인 테스팅 속도를 낼 수 있지 않을까?
+    // 보는 바와 같이 JOKER11과 비교하는데 아무런 에러 없이 테스트가 통과된다. 실제 Service에서는 "JOKER"를 넘기면
+    // Hello, JOKER를 반환한다.
+    @Before
+    public void mockGreetingService() {
+
+        given(greetingService.getMessage(null)).willReturn("Hello");
+        given(greetingService.getMessage("JOKER")).willReturn("Hello, JOKER11");
+    }
 
     @Test
     public void hello() throws Exception {

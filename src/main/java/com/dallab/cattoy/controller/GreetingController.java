a665e5e15656c1fdc1b8bf43db8af02ed5698c92
monkeyDugi <lbd4946@gmail.com>
2019-10-24 00:23:45 +0900

    Greeting명을 domain에게 양보하기 위하여 GreetingDto로 변경

--- src/main/java/com/dallab/cattoy/controller/GreetingController.java
+++ src/main/java/com/dallab/cattoy/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package com.dallab.cattoy.controller;
 
 import com.dallab.cattoy.application.GreetingService;
-import com.dallab.cattoy.dto.Greeting;
+import com.dallab.cattoy.dto.GreetingDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,12 +14,12 @@ public class GreetingController {
     private GreetingService greetingService;
 
     @GetMapping("/hello")
-    public Greeting hello(
+    public GreetingDto hello(
             // @RequestParam(defaultValue = "world") : param이 null 이면 "world" 있으면 name
             // @RequestParam(required = false) : null 아니야! 라고 강제
             @RequestParam(required = false) String name
     ) {
-        Greeting greeting = new Greeting();
+        GreetingDto greeting = new GreetingDto();
         greeting.setName("dugi");
         // Serivce의 메서드를 가져옴. 즉, 컨트롤러(UI)레이어에서 Service(Application)레이어를 사용
         greeting.setMessage(greetingService.getMessage(name));
